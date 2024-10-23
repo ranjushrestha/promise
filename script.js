ea8726fa6722ef779834c19dd16a79e9aedb962b
ranjustha <ranjustha8844@gmail.com>
2024-10-23 10:55:46 +0545

    added order summary and update wallet

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-const cart = ["shoes", "pants", "shirt"];
+// const cart = ["shoes", "pants", "shirt"];
 
 
-createOrder(cart)
-.then(function (orderId) {
-  console.log(orderId);
-  return orderId;
-})
-.then(function (orderId) {
-  return proceedToPayment(orderId);
-})
-.then(function (paymentInfo) {
-  console.log(paymentInfo);
-})
-.catch(function (err) {
-  console.log(err.message);
-});
+// createOrder(cart)
+// .then(function (orderId) {
+//   console.log(orderId);
+//   return orderId;
+// })
+// .then(function (orderId) {
+//   return proceedToPayment(orderId);
+// })
+// .then(function (paymentInfo) {
+//   console.log(paymentInfo);
+// })
+// .catch(function (err) {
+//   console.log(err.message);
+// });
 
-function createOrder(cart) {
-  return new Promise (function (res, rej) {
-    // createOrder
-    //validateCart
-    //orderId
-    if(!validateCart(cart)) {
-      const err = new Error("cart is not valid");
-      rej(err);
-    }
-    // logic for createOrder
-    const orderId = "585858";
-    if(orderId) {
-      setTimeout(function () {
-        res(orderId);
-      }, 5000)
+// function createOrder(cart) {
+//   return new Promise (function (res, rej) {
+//     // createOrder
+//     //validateCart
+//     //orderId
+//     if(!validateCart(cart)) {
+//       const err = new Error("cart is not valid");
+//       rej(err);
+//     }
+//     // logic for createOrder
+//     const orderId = "585858";
+//     if(orderId) {
+//       setTimeout(function () {
+//         res(orderId);
+//       }, 5000)
      
-    }
+//     }
 
 
-  });
+//   });
 
   
-}
+// }
 
-function proceedToPayment(orderId) {
-  ///proceedToPayment
-  return new Promise (function(res, rej) {
-      res("payment successful")
-  });
-}
+// function proceedToPayment(orderId) {
+//   ///proceedToPayment
+//   return new Promise (function(res, rej) {
+//       res("payment successful")
+//   });
+// }
 
-function validateCart(cart) {
-  return true;
-}
+// function validateCart(cart) {
+//   return true;
+// }
 
